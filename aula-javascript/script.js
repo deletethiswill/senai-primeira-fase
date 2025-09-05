5f6daf4a00b69820c84a886789d8364306733900
deletethiswill <hudsonwillianolivieira@gmail.com>
2025-09-04 21:54:54 -0300

    Mais sobre javascript e hmtl

---
+++ aula-javascript/script.js
@@ -0,0 +1,20 @@
+// alert("Olá mundo!")
+// alert("Oi")
+// alert("Tudo bem?")
+// alert("Você vem sempre aqui?")
+
+// let idade 
+// idade = 18
+// alert(idade)
+
+// idade = "dezoito"
+// alert(idade)
+
+// idade = 18.1
+// alert(idade)
+
+const amor = "meu primeiro amor"
+alert(amor)
+
+amor = "meu segundo amor"
+alert(amor)
