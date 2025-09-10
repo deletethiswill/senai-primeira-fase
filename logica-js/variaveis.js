3e7f9d07a965d76df1424c0eebb2918d017f44ec
deletethiswill <hudsonwillianolivieira@gmail.com>
2025-09-09 22:14:32 -0300

    variaveis em javascript''

---
+++ logica-js/variaveis.js
@@ -0,0 +1,23 @@
+// variavel com var
+// var nome = "Willian" // string 
+// var nome = "Hudson" // string
+
+// variavel com let
+// let nome = "willian" //string
+// nome = "hudson" //string
+
+// variavel decladara com const 
+// const nome = "WILL" //string
+// const sobrenome = "MANEIRÃO" //string
+// const idade = 19 //number
+// const masculino = true //boolean
+
+// console.log("ME CHAMAM DE " + nome + " " + sobrenome + " E TENHO " + idade + " ANOS DE IDADE")
+
+// console.log(`MEU NOME É ${nome} ${sobrenome} E MINHA IDADE É ${idade} ANOS`)
+
+//array
+
+const frutas = ["melancia", "uva", "banana"]
+
+console.log(frutas[1])
