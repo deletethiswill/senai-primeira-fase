9d70989f5af4fd99f1bef6664404f8e3f2032120
deletethiswill <hudsonwillianolivieira@gmail.com>
2025-09-10 22:21:42 -0300

    Coisinhas a mais

---
+++ sq02/ex01.js
@@ -0,0 +1,14 @@
+let idade
+
+idade = Number(prompt("Digite sua idade"))
+idade = idade + 1
+
+if(idade < 18){
+ alert("Menor de idade")
+ alert("Pode sair pro recreio")
+}else{(idade >= 18)
+    alert("Maior de idade")
+    alert("Pode sair pro intervalo")
+}
+
+console.log(idade)
---
+++ sq02/sq03/ex02.js
@@ -0,0 +1,7 @@
+let numero
+numero = Number(prompt("Digite um número"))
+if (numero % 2 == 0){
+alert("É PAR POHA")
+}else{
+alert("NÃO É PAR CARA, MEU DEUS")
+}
