10245c30c37741c33643e95837b2d1d65de50c89
deletethiswill <hudsonwillianolivieira@gmail.com>
2025-09-15 22:09:45 -0300

    Atividades do Java

---
+++ logica-js/sq04/ex03.js
@@ -0,0 +1,7 @@
+let senha
+senha = prompt("Digite sua senha:")
+if (senha == 1234){
+    alert("Senha Correta")
+}else{
+    alert("Senha Incorreta")
+}
---
+++ logica-js/sq05/ex05.js
@@ -0,0 +1,12 @@
+let num1, num2
+num1 = Number(prompt("DIGITE A POHA DO NÚMERO"))
+num2 = Number(prompt("DIGITE O OTO NÚMERO"))
+if (num1 > num2){
+    alert(num1 + "É MAIOR QUE" + num2) 
+}else if(num2 > num1){
+    alert(num2 + "É MAIOR QUE" + num1)
+}else{
+    alert("NÚMEROS IGUAIS")
+}
+
+
