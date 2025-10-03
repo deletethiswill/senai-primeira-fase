3208bd643d370a763784101665e5e8401419f800
deletethiswill <hudsonwillianolivieira@gmail.com>
2025-10-02 21:27:40 -0300

    Ablublueble

---
+++ logica-js/repeticoes/for.js
@@ -0,0 +1,13 @@
+// let i = 0 
+// while(i<3){
+//     alert("Valor do I: " + i )
+//     i++
+// }
+// for(let i = 0; i<3; i++){
+//     alert("Valor do I: " + i )
+// }
+
+const nomes = ["Gabriel", "SisifusPrime", "MinusPrime", "V2", "Benjamin"]
+for (let i =0; i<4; i++){
+    alert(nomes[i])
+}
