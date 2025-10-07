1af302e79b2ee38ae3159fd2b62738bb9e70995e
deletethiswill <hudsonwillianolivieira@gmail.com>
2025-10-06 22:00:14 -0300

    Coisinhas a mais

--- logica-js/repeticoes/for.js
+++ logica-js/repeticoes/for.js
@@ -7,7 +7,25 @@
 //     alert("Valor do I: " + i )
 // }
 
-const nomes = ["Gabriel", "SisifusPrime", "MinusPrime", "V2", "Benjamin"]
-for (let i =0; i<4; i++){
-    alert(nomes[i])
-}
+// const nomes = ["Gabriel", "SisifusPrime", "MinusPrime", "V2", "Benjamin"]
+// for (let i =0; i<4; i++){
+//     alert(nomes[i])
+// }
+
+// Calcule a soma dos números de 1 a 100
+
+// let soma = 0
+
+// for(let i=1; i<=100; i++){
+//     console.log(i)}
+
+    // let i=1
+    // while(i<=100){
+    //     console.log(i)
+    //     i++
+    // }
+    // let numero = Number(prompt("Tabuada de quê?"))
+    // for(let i=1; i<=10; i++){
+    // let resultado = i * numero
+    // console.log (i + "x" + numero + "=" + resultado);
+    // }
